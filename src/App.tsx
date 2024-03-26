461416899fc2c6b9d696448c37214f8293065525
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-03-26 09:38:18 +0100

    setup firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import db from "./utils/db/config/firebase";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+  useEffect(() => {
+    (async () => {
+      const testDocs = await getDocs(collection(db, "test"));
+      const data = testDocs.docs.map((doc) => doc.data());
+      console.log(data);
+    })();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button title="button" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
