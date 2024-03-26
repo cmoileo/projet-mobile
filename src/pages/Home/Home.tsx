52932b5a7c923586468a133193cd6339e21ad52a
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-03-26 11:05:07 +0100

    styl training pill

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,6 +7,7 @@ import {RemoveButton} from "../../components/RemoveButton/RemoveButton.component
 import Svg from 'react-native-svg';
 import {EditButton} from "../../components/EditButton/EditButton.component";
 import {InputTextComponent} from "../../components/InputText/InputText.component";
+import {TrainingPill} from "../../components/TrainingPill/TrainingPill.component";
 
 type Props = {
     navigation: NavigationProp<any>;
@@ -23,6 +24,7 @@ export const Home: React.FC<Props> = ({navigation}) => {
                 <StatusBar style="auto"/>
                 <AddButtonComponent onTap={handleAddButtonTap} />
                 <InputTextComponent />
+                <TrainingPill />
                 <RemoveButton onTap={handleAddButtonTap} />
                 <EditButton onTap={handleAddButtonTap} />
             </View>
