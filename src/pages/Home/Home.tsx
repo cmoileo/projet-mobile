43f13611a84a31fa81e66f5e3bc54643cdda340f
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-03-27 09:56:58 +0100

    wip

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,47 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {NavBar} from "../../components/NavBar/NavBar";
-import {NavigationProp} from "@react-navigation/native";
-import {AddButtonComponent} from "../../components/AddButton/AddButton.component";
-import {RemoveButton} from "../../components/RemoveButton/RemoveButton.component";
-import Svg from 'react-native-svg';
-import {EditButton} from "../../components/EditButton/EditButton.component";
-import {InputTextComponent} from "../../components/InputText/InputText.component";
-import {TrainingPill} from "../../components/TrainingPill/TrainingPill.component";
-import {ExercicePillComponent} from "../../components/ExercicePill/ExercicePill.component";
-import {ChronoComponent} from "../../components/Chronometre/Chrono.component";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { NavBar } from "../../components/NavBar/NavBar";
+import { NavigationProp } from "@react-navigation/native";
+import Chart from "../../components/Chart/Chart";
 
 type Props = {
-    navigation: NavigationProp<any>;
+  navigation: NavigationProp<any>;
 };
 
-export const Home: React.FC<Props> = ({navigation}) => {
-    const handleAddButtonTap = () => {
-        console.log("Add button tapped");
-    }
-    return (
-        <>
-            <View style={styles.container}>
-                <Text>Home page !</Text>
-                <StatusBar style="auto"/>
-                <AddButtonComponent onTap={handleAddButtonTap} />
-                <InputTextComponent />
-                <ExercicePillComponent />
-                <ChronoComponent />
-                <TrainingPill />
-                <RemoveButton onTap={handleAddButtonTap} />
-                <EditButton onTap={handleAddButtonTap} />
-            </View>
-            <NavBar navigation={navigation} />
-        </>
-    );
-}
+export const Home: React.FC<Props> = ({ navigation }) => {
+  return (
+    <>
+      <View style={styles.container}>
+
+        <Chart />
+        <StatusBar style="auto" />
+      </View>
+      <NavBar navigation={navigation} />
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
