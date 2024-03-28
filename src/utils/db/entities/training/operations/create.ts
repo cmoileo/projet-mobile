11b1886b1715fef3f17655651cc14112e052fb11
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-03-28 16:51:26 +0100

    fix error

--- src/utils/db/entities/training/operations/create.ts
+++ src/utils/db/entities/training/operations/create.ts
@@ -1,4 +1,4 @@
-import {collection, doc, setDoc, addDoc} from "firebase/firestore";
+import { collection, doc, setDoc, addDoc } from "firebase/firestore";
 import db from "../../../config/firebase";
 import { CreateTrainingDto } from "../../../dto/training/CreateTrainingDto";
 import ExercicesData from "../../../../../../assets/datas/exercices.json";
@@ -7,27 +7,21 @@ const create = async (data: CreateTrainingDto) => {
   try {
     const newDocRef = collection(db, "trainings");
     const newTraining = {
-      exercises: data.exercises.map(
-          (exercise) => {
-            const createdExercise = {
-              id: exercise.id,
-              date: exercise.date,
-              repetions: exercise.repetions,
-              weight: exercise.weight,
-              breakTime: exercise.breakTime,
-              perf: [],
-            }
-            return createdExercise;
-          }
-      ),
+      exercises: data.exercises.map((exercise) => {
+        const createdExercise = {
+          id: exercise.id,
+          perf: [],
+        };
+        return createdExercise;
+      }),
       userId: data.userId,
       name: data.name,
       date: data.date,
-    }
+    };
     const docRef = await addDoc(newDocRef, Object(newTraining));
     return docRef;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     throw error;
   }
 };
